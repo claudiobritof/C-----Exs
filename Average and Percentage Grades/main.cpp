266b288b2d3a94bfeada9ea8b9a6b1e67b6047af
Cláudio Brito <cabf.brito@gmail.com>
2022-10-25 10:50:26 -0300

    Average and Percentage Grades cpp

---
+++ Average and Percentage Grades/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int i, mark[5];
+    float sum=0, avg, perc;
+    cout<<"Enter 5 grades: ";
+    for(i=0; i<5; i++)
+    {
+        cin>>mark[i];
+        sum = sum+mark[i];
+    }
+    avg = sum/5;
+    perc = (sum/500)*100;
+    cout<<"\nAverage Marks = "<<avg;
+    cout<<"\nPercentage Marks = "<<perc<<"%";
+    cout<<endl;
+    return 0;
+}
